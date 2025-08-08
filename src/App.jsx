b075de3aace36b67ba4d29ed0690cbe7d704dbd3
Ayar Nasiem Mohammed <98074275+AyarNasiem@users.noreply.github.com>
2025-08-09 00:55:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ export default function App() {
           setLoading(true);
           setError(""); // for search
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal}
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal}
           );
           if (!res.ok)
             throw new Error("something went wrong with fetching movies");
